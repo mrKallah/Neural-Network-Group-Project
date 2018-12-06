6ebfed204ff7567f40d13175a747d1b0a9cf93b5
mrKallah <lasse_sortland@hotmail.com>
2018-12-06 10:34:45 +0000

    Added missing comments

--- autoencoder.py
+++ autoencoder.py
@@ -6,77 +6,77 @@ import csv
 
 
 def read_data(file):
-	'''
-	reads a csv file into a header array and a indata array
+	"""
+	reads a csv file into a header array and a data array
 	@author: Lasse Falch Sortland
 	:param file: the filename to read
-	:return: header array of type string with the header values and indata array of type float with the data values
-	'''
-	indata = []
+	:return: header array of type string with the header values and data array of type float with the data values
+	"""
+	data = []
 	header = []
 	with open(file) as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
-		couter = 0
+		counter = 0
 		for row in csv_reader:
-			if couter == 0:
+			if counter == 0:
 				header = row
 			else:
 				i = 0
 				for r in row:
 					row[i] = float(r)
 					i += 1
-				indata.append(row)
-			couter += 1
-	return header, indata
+				data.append(row)
+			counter += 1
+	return header, data
 
 
-def normalize(x, max, min, allowDivByZero=True):
-	'''
+def normalize(x, maximum, minimum, allow_div_by_zero=True):
+	"""
 	Normalizes a value x based on min and max
 	@author: Lasse Falch Sortland
 	:param x: the value to be normalized
-	:param min: the minimum value in the range
-	:param max: the maximum value in the range
-	:param allowDivByZero: if max is equal to min it returns 0 when true and throws error if false
+	:param minimum: the minimum value in the range
+	:param maximum: the maximum value in the range
+	:param allow_div_by_zero: if max is equal to min it returns 0 when true and throws error if false
 	:return: the normalized value as float
-	'''
-	if (max-min == 0):
-		if (allowDivByZero):
+	"""
+	if maximum-minimum == 0:
+		if allow_div_by_zero:
 			return 0
 		else:
 			raise TypeError('Division by zero')
-	return float(1-((max - x) / (max - min)))
+	return float(1 - ((maximum - x) / (maximum - minimum)))
 
 
-def normalizeArr(trainingData):
-	'''
+def normalize_array(training_data):
+	"""
 	Normalizes an array based on the max and min values of each column
 	@author: Lasse Falch Sortland
-	:param trainingData:
+	:param training_data:
 	:return: return a normalized version of the input array and the max and min values
-	'''
+	"""
 	# normalize
 	col = 0
 	maximum = []
 	minimum = []
-	size = np.asarray(trainingData[0]).shape[0]
+	size = np.asarray(training_data[0]).shape[0]
 	for val in range(size):
-		maximum.append(max(trainingData[:, val]))
-		minimum.append(min(trainingData[:, val]))
-	for trainDat in trainingData:
+		maximum.append(max(training_data[:, val]))
+		minimum.append(min(training_data[:, val]))
+	for trainDat in training_data:
 		row = 0
 		for dat in trainDat:
-			#maximum = np.max(trainingData[:, row])
-			#minimum = np.min(trainingData[:, row])
-			trainingData[col][row] = normalize(float(dat), maximum[row], minimum[row])
+			# maximum = np.max(training_data[:, row])
+			# minimum = np.min(training_data[:, row])
+			training_data[col][row] = normalize(float(dat), maximum[row], minimum[row])
 			row += 1
 		col += 1
-	return trainingData, maximum, minimum
+	return training_data, maximum, minimum
 
 
 def prepare_data(X_train_dir='dnnData/X_train.csv', y_train_dir='dnnData/y_train.csv',
                  X_test_dir='dnnData/X_test.csv', validation_size=100):
-	'''
+	"""
 	Converts csv files into arrays structured and ready for use in the auto encoder
 	@author Lasse Falch Sortland
 	:param X_train_dir: The location of the training csv file with missing data
@@ -86,7 +86,7 @@ def prepare_data(X_train_dir='dnnData/X_train.csv', y_train_dir='dnnData/y_train
 	:return: X_train, X_test, X_val, y_train, y_val as arrays of normalized values
 			 y_train_maximum, y_train_minimum, X_train_maximum, X_train_minimum, X_test_maximum, X_test_minimum
 			 which are can be used to denormalize the arrays again
-	'''
+	"""
 	print("reading data")
 	X_header, X_train = read_data(X_train_dir)
 	y_header, y_train = read_data(y_train_dir)
@@ -99,11 +99,11 @@ def prepare_data(X_train_dir='dnnData/X_train.csv', y_train_dir='dnnData/y_train
 
 	print("Normalize arrays")
 	# Scales the training and test data to range between 0 and 1.
-	y_train, y_train_maximum, y_train_minimum = normalizeArr(y_train)
+	y_train, y_train_maximum, y_train_minimum = normalize_array(y_train)
 	print("\ty_train done")
-	X_train, X_train_maximum, X_train_minimum = normalizeArr(X_train)
+	X_train, X_train_maximum, X_train_minimum = normalize_array(X_train)
 	print("\tX_train done")
-	X_test, X_test_maximum, X_test_minimum = normalizeArr(X_test)
+	X_test, X_test_maximum, X_test_minimum = normalize_array(X_test)
 	print("\tX_test done")
 
 	print("reshape arrays")
@@ -128,8 +128,9 @@ def auto_encode(X_train, X_test, X_val, y_train, y_val, layer1=4, encoding_dim=3
                 activation_in_encoder_layer_3='relu', activation_in_decoder_layer_1='relu',
                 activation_in_decoder_layer_2='relu', activation_in_decoder_layer_3='sigmoid',
                 optimizer='adam', loss='binary_crossentropy', epochs=10, batch_size=256):
-	'''
-
+	"""
+	creates an autoencoder, trains it and uses it to predict some data
+	@author: Lasse Falch Sortland
 	:param X_train: the unlabeled training data as arr
 	:param X_test: the unlabeled testing daata as arr
 	:param X_val: the unlabeled validation data as arr
@@ -160,7 +161,7 @@ def auto_encode(X_train, X_test, X_val, y_train, y_val, layer1=4, encoding_dim=3
 			 validation_prediction: the predicted values for the validation array
 			 validation_true: the actual values for the validation array
 			 test_predictions: the predicted values for the validation array
-	'''
+	"""
 
 	np.random.seed(1)
 	input_dim = X_train.shape[1]
@@ -208,12 +209,20 @@ def auto_encode(X_train, X_test, X_val, y_train, y_val, layer1=4, encoding_dim=3
 	print("end")
 	return validation_evaluation, validation_prediction, validation_true, test_predictions
 
+
 def main():
+	"""
+	This is only used for running the program standalone. Only for testing and developing purposes.
+	@author: Lasse Falch Sortland
+	:return: None
+	"""
 	X_train, X_test, X_val, y_train, y_val, y_train_maximum, y_train_minimum, \
 		X_train_maximum, X_train_minimum, X_test_maximum, X_test_minimum = prepare_data()
 	auto_encode(X_train, X_test, X_val, y_train, y_val, epochs=1)
 
 
 if __name__ == "__main__":
-	# stuff only to run when not called via 'import' here
+	'''
+	This will not be run when imported to another library only if run from this one itself.
+	'''
 	main()
